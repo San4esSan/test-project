b327c800f8221411b44d2c3a4e4074f8870691d6
San4es <mister.San4es@list.ru>
2020-01-22 20:32:48 +0400

    Added Gulp and SCSS

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const watch = require("gulp-watch");
+const sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./src/scss/main.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./src/css/"))
+    .pipe(browserSync.stream());
+});
+
+// Поднятие локального сервера
+gulp.task("server", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./src/"
+    }
+  });
+});
+
+// Следим за файлами и обновляем браузер
+gulp.task("watch", function() {
+  watch(
+    ["./src/*.html", "./src/js/*.js", "./src/img/*.*"],
+    gulp.parallel(browserSync.reload)
+  );
+
+  watch("./src/scss/**/*.sass", function() {
+    // setTimeout(gulp.parallel("scss"), 1000);
+    gulp.parallel("scss");
+  });
+});
+
+// Запускаем дефолтный таск
+gulp.task("default", gulp.series("sass", gulp.parallel("server", "watch")));
